e423e1d7e1bcfd99f43c984e2420cf5924549b60
Tardigrade-Died <sudhansudhan148@gmail.com>
2025-08-26 09:07:17 +0530

    Remove all files from main

--- Areas/Admin/Repositories/IAdminRepository.cs
+++
@@ -1,9 +0,0 @@
-using Ecommerce.Areas.Admin.Models;
-
-namespace Ecommerce.Areas.Admin.Repositories
-{
-    public interface IAdminRepository
-    {
-        AdminModel GetAdminByUsername(string username);
-    }
-}
